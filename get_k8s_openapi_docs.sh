a7ac6b60c027d2c96afee55af66da842c02da79b
Jeremy Choi <jechoi@redhat.com>
2023-07-18 19:51:51 -0400

    support to fetch only docs whose API group name include a substring

--- get_k8s_openapi_docs.sh
+++ get_k8s_openapi_docs.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
-#########################################################################
+############################################################################
 ##  Fetch OpenAPI docs of the Kubernetes APIs from the running cluster
 ##  Author: Jeremy Choi (jechoi@redhat.com)
-#########################################################################
+## 
+##  Usage: 
+##    - Fetch all the OpenAPI docs: $ ./get_k8s_openapi_docs.sh
+##    - Fetch an API group's doc:   $ ./get_k8s_openapi_docs.sh <substring-of-API-group>
+############################################################################
  
 PORT=7777
 APIDOCS_DIR=apidocs
+
+if [ "$#" -gt 1 ]; then
+  echo "Too many arguments"
+  echo "Usage:"
+  echo " - Fetch all the OpenAPI docs: $ ./get_k8s_openapi_docs.sh"
+  echo " - Fetch docs for API groups that contain a string: $ ./get_k8s_openapi_docs.sh <substring-of-API-group>"
+  exit
+fi
 
 mkdir -p $APIDOCS_DIR
 
@@ -14,7 +26,23 @@ mkdir -p $APIDOCS_DIR
 (kubectl proxy --port=$PORT &) | grep -q "Starting to serve on"
 echo "The proxy has been created on port $PORT"
 
+if [ ! -z "$1" ]; then
+  group_name=$1
+  echo "You've set to download the API group: $group_name"
+  group_found=false
+else
+  echo "You've set to download all the OpenAPI group: $group_name"
+fi
+
 for apipath in $(kubectl api-versions); do
+
+  if [ ! -z "$group_name" ]; then
+    if [[ $apipath != *"$group_name"* ]]; then
+      continue
+    else
+      group_found=true
+    fi
+  fi
 
   ## save openAPI files
   if [[ $apipath == *"/"* ]]    # e.g. 'node.k8s.io/v1'
@@ -34,12 +62,16 @@ for apipath in $(kubectl api-versions); do
   http_status=$(curl -s -o /dev/null -w "%{http_code}" "$download_url")
 
   if [[ $http_status -eq 200 ]]; then
-  curl -s $download_url > $result_filepath
+    curl -s $download_url > $result_filepath
   else 
-  echo "The Openapi doc is not found in the URL: $download_url"
+    echo "The Openapi doc is not found in the URL: $download_url"
   fi
 
 done
+
+if [ ! -z "$group_name" ] && [ "$group_found" = false ]; then
+  echo "No '$group_name' API found in the cluster"
+fi
 
 ## stop the proxy
 echo "Stopping the proxy"
